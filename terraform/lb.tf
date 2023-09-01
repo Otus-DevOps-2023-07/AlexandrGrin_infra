668be95a1de25cb3a1d68a17c2667b76d5563ac2
aleksandr-grishin <slip999@mail.ru>
2023-09-01 17:46:37 +0300

    terraform-1 HW

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,83 +1,35 @@
-resource "yandex_alb_target_group" "app-target-group" {
-  name = "my-target-group"
+resource "yandex_lb_network_load_balancer" "lb" {
+  name = "reddit-lb"
 
-  target {
-    subnet_id  = var.subnet_id
-    ip_address = yandex_compute_instance.app[0].network_interface.0.ip_address
-  }
-
-  target {
-    subnet_id  = var.subnet_id
-    ip_address = yandex_compute_instance.app[1].network_interface.0.ip_address
-  }
-}
-
-resource "yandex_alb_backend_group" "app-backend-group" {
-  name = "my-backend-group"
-
-  http_backend {
-    name             = "app-http-backend"
-    weight           = 1
-    port             = 9292
-    target_group_ids = ["${yandex_alb_target_group.app-target-group.id}"]
-    load_balancing_config {
-      panic_threshold = 0
+  listener {
+    name        = "reddit-listener"
+    port        = 9292
+    target_port = 9292
+    protocol    = "tcp"
+    external_address_spec {
+      ip_version = "ipv4"
     }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.tg.id
     healthcheck {
-      healthcheck_port = 9292
-      timeout          = "1s"
-      interval         = "1s"
-      http_healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
         path = "/"
       }
     }
   }
 }
 
-resource "yandex_alb_http_router" "tf-router" {
-  name = "first-http-router"
-}
-
-resource "yandex_alb_virtual_host" "app-virtual-host" {
-  name           = "my-virtual-host"
-  http_router_id = yandex_alb_http_router.tf-router.id
-  route {
-    name = "app-route"
-    http_route {
-      http_route_action {
-        backend_group_id = yandex_alb_backend_group.app-backend-group.id
-        timeout          = "3s"
-      }
-    }
-  }
-}
-
-resource "yandex_alb_load_balancer" "load_balancer" {
-  name = "first-load-balancer"
-
-  network_id = var.network_id
-
-  allocation_policy {
-    location {
-      zone_id   = "ru-central1-a"
+resource "yandex_lb_target_group" "tg" {
+  name = "reddit-tg"
+  dynamic "target" {
+    for_each = yandex_compute_instance.app.*.network_interface.0.ip_address
+    content {
+      address   = target.value
       subnet_id = var.subnet_id
     }
   }
-
-  listener {
-    name = "my-listener"
-    endpoint {
-      address {
-        external_ipv4_address {
-        }
-      }
-      ports = [9292]
-    }
-    http {
-      handler {
-        http_router_id = yandex_alb_http_router.tf-router.id
-      }
-    }
-  }
-
 }
