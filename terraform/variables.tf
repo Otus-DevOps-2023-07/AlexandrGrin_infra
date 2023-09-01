22b3bc5d8425fd24b7c9da10ccf844ea84fbd970
aleksandr-grishin <slip999@mail.ru>
2023-09-01 16:57:36 +0300

    terraform-1 HW

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "cloud_id" {
+  description = "Cloud"
+}
+
+variable "folder_id" {
+  description = "Folder"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = ""
+}
+
+variable "public_key_path" {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "image_id" {
+  description = "Disk image"
+}
+
+variable "network_id" {
+  description = "Network ID"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+}
+
+variable "service_account_key_file" {
+  description = "key.json"
+}
+
+variable "privite_key" {
+  description = "privite_key"
+}
